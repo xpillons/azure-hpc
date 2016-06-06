30e08a0b96791036f9e824426cf6bce72deddb8b
Xavier Pillons <xpillons@microsoft.com>
2016-06-06 19:05:38 +0200

    Added SA info and Azure Login info

--- Compute-Grid-Infra/master-setup.sh
+++ Compute-Grid-Infra/master-setup.sh
@@ -1,2 +1,60 @@
 #bash 
-yum update -y
+SA_NAME="account"
+SA_KEY="key"
+APP_ID=""
+TENAND_ID=""
+PASSWORD=""
+
+#############################################################################
+log()
+{
+	echo "$1"
+}
+
+while getopts :a:k:u:tp optname; do
+  log "Option $optname set with value ${OPTARG}"
+  
+  case $optname in
+    a)  # storage account
+		SA_NAME=${OPTARG}
+		;;
+    k)  # storage key
+		SA_KEY=${OPTARG}
+		;;
+    u)  # user id
+		APP_ID=${OPTARG}
+		;;
+    t)  # tenand id
+		TENAND_ID=${OPTARG}
+		;;
+    p)  # password
+		PASSWORD=${OPTARG}
+		;;
+  esac
+done
+
+
+######################################################################
+install_azure_cli()
+{
+	curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -
+	yum -y install nodejs
+
+	npm install -g azure-cli
+}
+
+######################################################################
+install_azure_files()
+{
+	yum -y install samba-client samba-common cifs-utils
+	mkdir /mnt/azure
+	
+	azure login -u $APP_ID --service-principal --tenant $TENAND_ID -p $PASSWORD
+	azure storage share create --share lsf -a $SA_NAME -k $SA_KEY
+	
+	mount -t cifs //$SA_NAME.file.core.windows.net/lsf /mnt/azure -o vers=3.0,username=$SA_NAME,password='$SA_KEY',dir_mode=0777,file_mode=0777
+	echo //$SA_NAME.file.core.windows.net/lsf /mnt/azure -o vers=3.0,username=$SA_NAME,password='$SA_KEY',dir_mode=0777,file_mode=0777 >> /etc/fstab
+}
+
+install_azure_cli
+install_azure_files
