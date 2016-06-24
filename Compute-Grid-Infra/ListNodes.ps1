6cf6e6c2154f33838d643d99b6874e9487eca393
Xavier Pillons <xpillons@microsoft.com>
2016-06-24 17:51:17 +0200

    Added

---
+++ Compute-Grid-Infra/ListNodes.ps1
@@ -0,0 +1,15 @@
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$ResourceGroupName
+)
+
+(Get-AzureRmVmss -ResourceGroupName $ResourceGroupName) | Foreach-Object {
+    $vmssName = $_.Name
+    $vms = Get-AzureRmVmssVM -ResourceGroupName $ResourceGroupName -VMScaleSetName $vmssName
+    $vms | Foreach-Object {
+        $vmIndex = $_.InstanceId
+        $vmName = $_.OsProfile.ComputerName
+        $ip = (Get-AzureRmNetworkInterface -ResourceGroupName $ResourceGroupName -VirtualMachineScaleSetName $vmssName -VirtualMachineIndex $vmIndex).IpConfigurations[0].PrivateIpAddress
+        Write-Host $vmName $ip
+    }
+}
