df1b9b51e4593226bcd0e6ab03251b3eef51792f
Xavier Pillons <xpillons@microsoft.com>
2016-07-04 16:45:31 +0200

    updated provision script

--- Compute-Grid-Infra/cn-setup.bat
+++ Compute-Grid-Infra/cn-setup.bat
@@ -1,5 +1,7 @@
 set MASTER_NAME=%1
 
+cmdkey /add:%COMPUTERNAME% /user:%2 /pass:%3
+
 net use Z: \\%MASTER_NAME%\Data /user:%2 %3
 
-cmd /c Z:\Symphony\provisionScript.bat
+runas /savecred /user:%COMPUTERNAME%\%2 "cmd /c Z:\Symphony\provisionScript.bat"
