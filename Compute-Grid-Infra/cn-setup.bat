c4726a1e0af6ed4bd726081283ca31e159a828ce
Xavier Pillons <xpillons@microsoft.com>
2016-07-04 18:14:48 +0200

    fix cmdkey

--- Compute-Grid-Infra/cn-setup.bat
+++ Compute-Grid-Infra/cn-setup.bat
@@ -1,8 +1,11 @@
 set MASTER_NAME=%1
 
-cmdkey /generic:%COMPUTERNAME% /user:%COMPUTERNAME%\%2 /pass:%3
+cmdkey /add:%COMPUTERNAME% /user:%COMPUTERNAME%\%2 /pass:%3
+cmdkey /add:%COMPUTERNAME% /user:%2 /pass:%3
+cmdkey /add:%COMPUTERNAME% /user:.\%2 /pass:%3
+
 cmdkey /list
 
 net use Z: \\%MASTER_NAME%\Data /user:%2 %3 /persistent:yes
 
-runas /savecred /user:%COMPUTERNAME%\%2 "cmd /c Z:\Symphony\provisionScript.bat"
+runas /savecred /user:%2 "cmd /c Z:\Symphony\provisionScript.bat"
