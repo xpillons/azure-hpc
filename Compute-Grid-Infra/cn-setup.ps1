ade9bc954fc3c10be740bf48c888f4421658823d
Xavier Pillons <xpillons@microsoft.com>
2016-07-04 20:37:28 +0200

    test

--- Compute-Grid-Infra/cn-setup.ps1
+++ Compute-Grid-Infra/cn-setup.ps1
@@ -8,9 +8,12 @@ param (
 )
 
 $User = ".\$UserName"
+Write-Host $User
 $PWord = ConvertTo-SecureString -String $Password -AsPlainText -Force
 $Credential = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $User, $PWord
 
-&net use Z: \\$MasterName\Data /user:$UserName $Password /persistent:yes
+&net use Z: \\$MasterName\Data /user:$UserName $Password /persistent:yes | Out-Host
+
+&net use | Out-Host
 
 Start-Process "C:\Windows\System32\cmd.exe" -WorkingDirectory "C:\" -Credential ($Credential) -ArgumentList " /c"
