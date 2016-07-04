3daa291c5f02bf84505da385c732f31464096b35
Xavier Pillons <xpillons@microsoft.com>
2016-07-04 20:15:50 +0200

    fix script

--- Compute-Grid-Infra/cn-setup.ps1
+++ Compute-Grid-Infra/cn-setup.ps1
@@ -13,4 +13,4 @@ $Credential = New-Object -TypeName "System.Management.Automation.PSCredential" -
 
 &net use Z: \\$MasterName\Data /user:$UserName $Password /persistent:yes
 
-Start-Process "C:\Windows\System32\cmd.exe" -WorkingDirectory "Z:\symphony" -Credential ($Credential) -ArgumentList "cmd /c provisionScript.bat"
+Start-Process "C:\Windows\System32\cmd.exe" -WorkingDirectory "C:\" -Credential ($Credential) -ArgumentList " /c"
