3705393859f6d422ce7aec42fa5794b9351dcea5
Xavier Pillons <xpillons@microsoft.com>
2016-09-22 10:50:12 +0200

    removed array of SA simplified install script, removed management and client, and install both storage and metadata

--- Compute-Grid-Infra/BeeGFS/install_beegfs.sh
+++ Compute-Grid-Infra/BeeGFS/install_beegfs.sh
@@ -8,54 +8,17 @@ if [[ $(id -u) -ne 0 ]] ; then
     exit 1
 fi
 
-if [ $# != 3 ]; then
-    echo "Usage: $0 <NodeCount> <StorageNodePrefix> <ManagementHost>"
+if [ $# != 1 ]; then
+    echo "Usage: $0 <ManagementHost>"
     exit 1
 fi
 
-# Set user args
-METADATA_COUNT=$1
-STORAGE_HOSTNAME_PREFIX=$2
-# Use the first storage server for management server
-MGMT_HOSTNAME=$3
+MGMT_HOSTNAME=$1
 
 # Shares
 SHARE_SCRATCH=/share/scratch
 BEEGFS_METADATA=/data/beegfs/meta
 BEEGFS_STORAGE=/data/beegfs/storage
-
-is_mgmtnode()
-{
-    hostname | grep "$MGMT_HOSTNAME"
-    return $?
-}
-
-is_metadatanode()
-{
-    lastMetadataNode=$(($METADATA_COUNT - 1))
-    
-    for i in $(seq 0 $lastMetadataNode); do
-        hostname | grep "${STORAGE_HOSTNAME_PREFIX}${i}"
-        if [ $? -eq 0 ]; then
-            return 0
-        fi
-    done
-    
-    # We're not a metadata node
-    return 1
-}
-
-is_storagenode()
-{
-    hostname | grep "$STORAGE_HOSTNAME_PREFIX"
-    return $?
-}
-
-is_beegfsnode()
-{
-    hostname | grep "$STORAGE_HOSTNAME_PREFIX"
-    return $?
-}
 
 # Installs all required packages.
 #
@@ -119,15 +82,13 @@ setup_disks()
     # Dump the scsi config
     lsscsi
 
-    if is_beegfsnode; then
-        # Configure metadata and storage disks
-        mkdir -p $BEEGFS_STORAGE
-        mkdir -p $BEEGFS_METADATA
+    # Configure metadata and storage disks
+    mkdir -p $BEEGFS_STORAGE
+    mkdir -p $BEEGFS_METADATA
 
-		# TODO : need to build the device list dynamically based on the number of disks required for storage and metadata
-        setup_data_disks $BEEGFS_STORAGE "xfs" "sdc sdd" "md10"
-        setup_data_disks $BEEGFS_METADATA "ext4" "sde sdf" "md20"
-    fi
+	# TODO : need to build the device list dynamically based on the number of disks required for storage and metadata
+    setup_data_disks $BEEGFS_STORAGE "xfs" "sdc sdd" "md10"
+    setup_data_disks $BEEGFS_METADATA "ext4" "sde sdf" "md20"
 
     mount -a
 }
@@ -142,45 +103,23 @@ install_beegfs()
     # Disable SELinux
     sed -i 's/SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
     setenforce 0
-
-    if is_mgmtnode; then
-        yum install -y beegfs-mgmtd beegfs-client beegfs-helperd beegfs-utils
-        
-        # Install management server and client
-        mkdir -p /data/beegfs/mgmtd
-        sed -i 's|^storeMgmtdDirectory.*|storeMgmtdDirectory = /data/beegfs/mgmt|g' /etc/beegfs/beegfs-mgmtd.conf
-        systemctl daemon-reload
-        systemctl enable beegfs-mgmtd.service
-        
-        # setup client
-        sed -i 's/^sysMgmtdHost.*/sysMgmtdHost = '$MGMT_HOSTNAME'/g' /etc/beegfs/beegfs-client.conf
-        sed -i  's/Type=oneshot.*/Type=oneshot\nRestart=always\nRestartSec=5/g' /etc/systemd/system/multi-user.target.wants/beegfs-client.service
-        echo "$SHARE_SCRATCH /etc/beegfs/beegfs-client.conf" > /etc/beegfs/beegfs-mounts.conf
-        systemctl daemon-reload
-        systemctl enable beegfs-helperd.service
-        systemctl enable beegfs-client.service
-    fi
     
 	# setup metata data
-    if is_beegfsnode; then
-        yum install -y beegfs-meta
-        sed -i 's|^storeMetaDirectory.*|storeMetaDirectory = '$BEEGFS_METADATA'|g' /etc/beegfs/beegfs-meta.conf
-        sed -i 's/^sysMgmtdHost.*/sysMgmtdHost = '$MGMT_HOSTNAME'/g' /etc/beegfs/beegfs-meta.conf
-        systemctl daemon-reload
-        systemctl enable beegfs-meta.service
+    yum install -y beegfs-meta
+    sed -i 's|^storeMetaDirectory.*|storeMetaDirectory = '$BEEGFS_METADATA'|g' /etc/beegfs/beegfs-meta.conf
+    sed -i 's/^sysMgmtdHost.*/sysMgmtdHost = '$MGMT_HOSTNAME'/g' /etc/beegfs/beegfs-meta.conf
+    systemctl daemon-reload
+    systemctl enable beegfs-meta.service
         
-        # See http://www.beegfs.com/wiki/MetaServerTuning#xattr
-        echo deadline > /sys/block/sdX/queue/scheduler
-    fi
+    # See http://www.beegfs.com/wiki/MetaServerTuning#xattr
+    echo deadline > /sys/block/sdX/queue/scheduler
     
 	# setup storage
-    if is_beegfsnode; then
-        yum install -y beegfs-storage
-        sed -i 's|^storeStorageDirectory.*|storeStorageDirectory = '$BEEGFS_STORAGE'|g' /etc/beegfs/beegfs-storage.conf
-        sed -i 's/^sysMgmtdHost.*/sysMgmtdHost = '$MGMT_HOSTNAME'/g' /etc/beegfs/beegfs-storage.conf
-        systemctl daemon-reload
-        systemctl enable beegfs-storage.service
-    fi
+    yum install -y beegfs-storage
+    sed -i 's|^storeStorageDirectory.*|storeStorageDirectory = '$BEEGFS_STORAGE'|g' /etc/beegfs/beegfs-storage.conf
+    sed -i 's/^sysMgmtdHost.*/sysMgmtdHost = '$MGMT_HOSTNAME'/g' /etc/beegfs/beegfs-storage.conf
+    systemctl daemon-reload
+    systemctl enable beegfs-storage.service
 }
 
 setup_swap()
