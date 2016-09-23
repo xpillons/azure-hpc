f0ef3cb8ecc9023da0539dd6172e5f7e278a64c2
Xavier Pillons <xpillons@microsoft.com>
2016-09-23 11:23:43 +0200

    added beegfs client setup

--- Compute-Grid-Infra/cn-setup.sh
+++ Compute-Grid-Infra/cn-setup.sh
@@ -42,7 +42,7 @@ install_azure_files()
 {
 	log "install samba and cifs utils"
 	yum -y install samba-client samba-common cifs-utils
-	mkdir /mnt/azure
+	mkdir -p ${MOUNT_POINT}
 	
 	log "mount share"
 	mount -t cifs //$AZURE_STORAGE_ACCOUNT.file.core.windows.net/lsf /mnt/azure -o vers=3.0,username=$AZURE_STORAGE_ACCOUNT,password=''${AZURE_STORAGE_ACCESS_KEY}'',dir_mode=0777,file_mode=0777
@@ -59,7 +59,7 @@ install_applications()
 mount_nfs()
 {
 	log "install NFS"
-#	yum -y update
+
 	yum -y install nfs-utils nfs-utils-lib
 	
 	mkdir -p ${MOUNT_POINT}
@@ -69,7 +69,41 @@ mount_nfs()
 	mount -t nfs ${MASTER_NAME}:/nfsdata/apps ${MOUNT_POINT}/	 
 }
 
+install_beegfs_client()
+{
+	SHARE_SCRATCH=/share/scratch
+	mkdir -p $SHARE_SCRATCH
+	MGMT_HOSTNAME=${MASTER_NAME}
+
+	yum -y install epel-release
+    yum -y install zlib zlib-devel bzip2 bzip2-devel bzip2-libs openssl openssl-devel openssl-libs gcc gcc-c++ nfs-utils rpcbind mdadm wget python-pip kernel kernel-devel openmpi openmpi-devel automake autoconf
+
+    # Install BeeGFS repo
+    wget -O beegfs-rhel7.repo http://www.beegfs.com/release/latest-stable/dists/beegfs-rhel7.repo
+    mv beegfs-rhel7.repo /etc/yum.repos.d/beegfs.repo
+    rpm --import http://www.beegfs.com/release/latest-stable/gpg/RPM-GPG-KEY-beegfs
+    
+    # Disable SELinux
+    sed -i 's/SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
+    setenforce 0
+
+    yum install -y beegfs-client beegfs-helperd beegfs-utils
+        
+    # setup client
+    sed -i 's/^sysMgmtdHost.*/sysMgmtdHost = '$MGMT_HOSTNAME'/g' /etc/beegfs/beegfs-client.conf
+    sed -i  's/Type=oneshot.*/Type=oneshot\nRestart=always\nRestartSec=5/g' /etc/systemd/system/multi-user.target.wants/beegfs-client.service
+    echo "$SHARE_SCRATCH /etc/beegfs/beegfs-client.conf" > /etc/beegfs/beegfs-mounts.conf
+    systemctl daemon-reload
+    systemctl enable beegfs-helperd.service
+    systemctl enable beegfs-client.service
+
+}
+
 #install_azure_cli
 #install_azure_files
-mount_nfs
-install_applications
+#mount_nfs
+#install_applications
+install_beegfs_client
+
+shutdown -r +1 &
+exit 0
