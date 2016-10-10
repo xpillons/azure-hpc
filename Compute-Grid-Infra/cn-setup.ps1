a073c5639dc3729c27ee8ad605e65aa8ea1f593c
Xavier Pillons <xpillons@microsoft.com>
2016-10-10 16:32:24 +0200

    removed invoke-command

--- Compute-Grid-Infra/cn-setup.ps1
+++ Compute-Grid-Infra/cn-setup.ps1
@@ -10,9 +10,19 @@ param (
 
 function RegisterReverseDNS($shareName)
 {
+	Write-Host "Register Reverse DNS $shareName"
+	$ip = test-connection $shareName -timetolive 2 -count 1 | Select -ExpandProperty IPV4Address 
+
+	Write-Host "$shareName is $ip.IPAddressToString"
+	$ip4Value = $ip.IPAddressToString
+
 	&reg add HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters /v Domain /d southcentralus.cloudapp.azure.com /f
 	&reg add HKLM\System\currentcontrolset\services\tcpip\parameters /v SearchList /d southcentralus.cloudapp.azure.com /f
-	&netsh interface ipv4 add dnsserver "Ethernet" address=10.0.8.4 index=1
+
+	Write-Host "running netsh"
+	&netsh interface ipv4 add dnsserver "Ethernet" address=$ip4Value index=1
+
+	Write-Host "running IPCONFIG"
 	&ipconfig /registerdns
 
 	#Import-Module ServerManager
@@ -27,15 +37,18 @@ function RegisterReverseDNS($shareName)
 	#Add-DnsServerResourceRecordPtr -ComputerName $shareName -Name $name -ZoneName $zone -PtrDomainName $env:COMPUTERNAME
 }
 
-function AddRunCommands()
+function AddRunCommands($shareName)
 {
+	$ip = test-connection $shareName -timetolive 2 -count 1 | Select -ExpandProperty IPV4Address 
+	$ip4Value = $ip.IPAddressToString
+
 	#$command = "reg add HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters /v Domain /d southcentralus.cloudapp.azure.com /f"
 	#&reg add HKLM\Software\Microsoft\Windows\CurrentVersion\Run /v Domain /f /d $command
 
 	#$command = "reg add HKLM\System\currentcontrolset\services\tcpip\parameters /v SearchList /d southcentralus.cloudapp.azure.com /f"
 	#&reg add HKLM\Software\Microsoft\Windows\CurrentVersion\Run /v SearchList /f /d $command
 	
-	$command = "netsh interface ipv4 add dnsserver 'Ethernet' address=10.0.8.4 index=1"
+	$command = "netsh interface ipv4 add dnsserver 'Ethernet' address=$ip4Value index=1"
 	&reg add HKLM\Software\Microsoft\Windows\CurrentVersion\Run /v adddns /f /d $command
 	
 	$command = "ipconfig /registerdns"
@@ -46,32 +59,48 @@ function AddRunCommands()
 
 function RunSetup($shareName, $user, $pwd)
 {
+	Write-Host "Run setup $shareName $user"
+	&net use Z: \\$shareName\Data /user:$user $pwd /persistent:yes 
+	&net use  
 
-	&net use Z: \\$shareName\Data /user:$user $pwd /persistent:yes | Out-Host
-	&net use | Out-Host 
-
-	&Z:\symphony\provisionScript.bat | Out-Host 
+	Write-Host "Run provisionScript"
+	&Z:\symphony\provisionScript.bat 
 }
 
 function Main()
 {
 	# Enable Remote Powershell Execution From The Master Node
 	# don't put these lines into the script called by the session because it will close the session :-)
-	Enable-PSRemoting -Force
-	$trustedHosts="@{TrustedHosts=\""$MasterName\""}"
+	try {
+		Write-Host "Enable-PSRemoting"
+		Enable-PSRemoting -Force
+		$trustedHosts="@{TrustedHosts=\""$MasterName\""}"
+		Write-Host "Config WinRM for $trustedHosts"
+		&winrm s winrm/config/client $trustedHosts
 
-	&winrm s winrm/config/client $trustedHosts
-	Restart-Service WinRM -Force
+		Write-Host "Restart WinRM"
+		Restart-Service WinRM -Force
+	}
+	catch {
 
-	AddRunCommands 
+	}
+	Write-Host "Add commands to run on startup"
+	AddRunCommands $MasterName
 
 	# Create local credential to run the installation script
-	$User = ".\$UserName"
-	$PWord = ConvertTo-SecureString -String $Password -AsPlainText -Force
-	$Credential = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $User, $PWord
+	#Write-Host "Create Local Credentials"
+	#$User = ".\$UserName"
+	#$PWord = ConvertTo-SecureString -String $Password -AsPlainText -Force
+	#$Credential = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $User, $PWord
 
-	$psSession = New-PSSession -Credential $Credential
-	Invoke-Command -Session $psSession -Script ${function:RunSetup} -ArgumentList $MasterName,$UserName,$Password
+	#Write-Host "Create New PS Session"
+	#$psSession = New-PSSession -Credential $Credential
+	#Write-Host "Invoke RunSetup"
+	#Invoke-Command -Session $psSession -Script ${function:RunSetup} -ArgumentList $MasterName,$UserName,$Password
+
+	RunSetup $MasterName $UserName $Password
+
+	Write-Host "Create Marker"
     New-Item -Path C:\ -Name customscript.txt -ItemType File
 }
 
@@ -79,11 +108,13 @@ RegisterReverseDNS $MasterName
 
 $touch = (Test-Path -Path C:\customscript.txt)
 
+Write-Host "Restart LIM"
 &net stop LIM
 &net start LIM
 
 if ($touch -eq $false)
 {
+	Write-Host "Marker file doesn't exists"
     Main
 }
 
