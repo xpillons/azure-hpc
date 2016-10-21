8530540f756a511601e80d29d73510f163592b3d
Xavier Pillons <xpillons@microsoft.com>
2016-10-21 13:50:29 +0200

    fix beeGFS client install

--- Compute-Grid-Infra/BeeGFS/install_beegfs_client.sh
+++ Compute-Grid-Infra/BeeGFS/install_beegfs_client.sh
@@ -47,10 +47,9 @@ install_beegfs()
         
     # setup client
     sed -i 's/^sysMgmtdHost.*/sysMgmtdHost = '$MGMT_HOSTNAME'/g' /etc/beegfs/beegfs-client.conf
-    #sed -i  's/Type=oneshot.*/Type=oneshot\nRestart=always\nRestartSec=5/g' /etc/systemd/system/multi-user.target.wants/beegfs-client.service
-    sed -i  's/Type=oneshot.*/Type=oneshot\nRestart=always\nRestartSec=5/g' /usr/lib/systemd/system/beegfs-client.service
-	
+    #sed -i  's/Type=oneshot.*/Type=oneshot\nRestart=always\nRestartSec=5/g' /usr/lib/systemd/system/beegfs-client.service	
     echo "$SHARE_SCRATCH /etc/beegfs/beegfs-client.conf" > /etc/beegfs/beegfs-mounts.conf
+	
     systemctl daemon-reload
     systemctl enable beegfs-helperd.service
     systemctl enable beegfs-client.service
