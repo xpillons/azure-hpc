4410de7a1f6a4a53f87d1078240ad2256bc7d31c
Xavier Pillons <xpillons@microsoft.com>
2016-11-23 17:18:16 +0100

    fix params

--- Compute-Grid-Infra/BeeGFS/install_beegfs.sh
+++ Compute-Grid-Infra/BeeGFS/install_beegfs.sh
@@ -8,7 +8,7 @@ if [[ $(id -u) -ne 0 ]] ; then
     exit 1
 fi
 
-if [ $# != 2 ]; then
+if [ $# < 2 ]; then
     echo "Usage: $0 <ManagementHost> <Type (meta,storage,both)> <customDomain>"
     exit 1
 fi
